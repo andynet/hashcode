8e1e8213d387f505eaa1fdc139c5a0cfe490fda3
lukaskiss222 <lukaskiss222@gmail.com>
2017-02-27 23:28:39 +0100

    Dokoncena Evalute v func.py
    Konecne som dokoncil evaluate vi func.py aj ked sa da este zrychlist to
    sa potom dorobi.
    Dalej som upravil run.py na testovanie
    
    cppy_load som len skratil zapis, bolo to zbytocne.
    02.in novy vstup na testovanie.

--- cy_python/cppy_load.pyx
+++ cy_python/cppy_load.pyx
@@ -1,6 +1,4 @@
 cimport cpp_load
 
 def read_input():
-    cdef cpp_load.google_input pom = cpp_load.read_input()
-    print(pom.num_videos)
-    return pom 
+    return cpp_load.read_input() 
--- python/run.py
+++ python/run.py
@@ -1,10 +1,14 @@
 import sys
 sys.path.append('../cy_python')
 import cppy_load as cl
-
+import func
 
 
 pom = cl.read_input()
+print(pom)
+for video_id in range(pom['num_videos']):
+    print("============================================")
+    for cache_id in range(pom['num_caches']):
+        vys = func.evaluate(pom['endpoints'],video_id,cache_id)
+        print("Video: %d, Cache: %d = %d" % (video_id,cache_id,vys))
 
-for a in pom:
-    print(pom[a])
